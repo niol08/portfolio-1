110c843df51930f611744fe05723eab2a5ece6ff
Eniola Oladejo <enioladejo1725@gmail.com>
2025-07-12 21:24:14 +0100

    updated repo-1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -145,7 +145,7 @@ const fontThemes = [
 ];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 2000;
 let textArrayIndex = 0;
 let charIndex = 0;
 
